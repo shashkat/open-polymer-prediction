22ac2abcb998eb8f38f6970b34ccfbb6b558dc47
AndrewLutsky <andrew.lutsky19@gmail.com>
2025-08-29 23:47:21 -0400

    dev0.0.3_al MLPRegressor

--- src/data/splitting.py
+++ src/data/splitting.py
@@ -19,14 +19,21 @@ def main(file:str,
         raise SplittingException()
 
     try:
-        # Read in file --- MUST BE CSV FILE
+        # Read in file --- MUST BE CSV FILE -- into memory
+        # maybe a bad choice if data is bigger...
         df = pd.read_csv(file)
+
+        # Create the fold column..
+        df['fold'] = -1
         
         # Create n folds.
-        folds = KFold(n_splits=folds)
-        splits = folds.split(df)
-        print(splits)
+        folds = KFold(n_splits=folds, shuffle=True, random_state=42)
+        for fold_idx, (_, val_idx) in enumerate(folds.split(df)):
+            df.loc[val_idx, "fold"] = fold_idx
 
+        # Write out dataframe.
+        df.to_csv(os.path.join(outdir, "folds.csv"), index=False)
+        print(f'Saved dataframe with {folds} folds to {outdir}.')
         return
     except Exception as e:
         raise SplittingException(e)
